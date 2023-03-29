0d50116ba30ff1d91772bdb039a7f5427f91568f
조준영 <litlejo@sungkyul.ac.kr>
2023-03-29 05:35:46 +0000

    ## 2023년 3월 29일 4주차 홈페이지 수정 완료

---
+++ .js/search.js
@@ -0,0 +1,12 @@
+<html>
+	<head>
+<script type="text/javascript" src="js/search.js" defer></script>
+	</head>
+	<body>
+		document.getElementById("search_btn").addEventListener('click', search_message);
+
+function search_message(){
+   alert("검색을 수행합니다!");
+}
+	</body>
+</html>
