6cf646effc0d4fc28bb0e4b354eb80776a7aa4b1
조준영 <litlejo@sungkyul.ac.kr>
2023-06-14 13:48:49 +0000

    6791011주차 문제

--- js/login.js
+++ js/login.js
@@ -110,3 +110,136 @@ function maintainLoginTime() {
 // 예시 사용법
 login();
 maintainLoginTime();*/
+
+function getCookieValue(cookieName) {
+    const name = cookieName + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const cookieArray = decodedCookie.split(";");
+
+    for (let i = 0; i < cookieArray.length; i++) {
+        let cookie = cookieArray[i];
+        while (cookie.charAt(0) === " ") {
+            cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf(name) === 0) {
+            return cookie.substring(name.length, cookie.length);
+        }
+    }
+
+    return "";
+}
+
+/*function setCookieValue(cookieName, value) {
+    document.cookie = cookieName + "=" + value;
+}
+
+function login_count() {
+    let count = parseInt(getCookieValue("login_cnt"));
+    count++;
+    setCookieValue("login_cnt", count);
+    console.log("로그인 횟수: " + count);
+}
+
+function logout_count() {
+    let count = parseInt(getCookieValue("logout_cnt"));
+    count++;
+    setCookieValue("logout_cnt", count);
+    console.log("로그아웃 횟수: " + count);
+}
+
+function login() {
+    login_count();
+    // 로그인 처리 추가 작업
+}
+
+function logout() {
+    logout_count();
+    // 로그아웃 처리 추가 작업
+}*/
+
+// login.js
+
+/*const loginTimeout = 5 * 60 * 1000; // 5분 (밀리초 단위)
+
+function getCookieValue(cookieName) {
+    const name = cookieName + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const cookieArray = decodedCookie.split(";");
+
+    for (let i = 0; i < cookieArray.length; i++) {
+        let cookie = cookieArray[i];
+        while (cookie.charAt(0) === " ") {
+            cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf(name) === 0) {
+            return cookie.substring(name.length, cookie.length);
+        }
+    }
+
+    return "";
+}
+
+function setCookieValue(cookieName, value) {
+    document.cookie = cookieName + "=" + value;
+}
+
+function deleteCookie(cookieName) {
+    document.cookie = cookieName + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+}
+
+function login_count() {
+    let count = parseInt(getCookieValue("login_cnt"));
+    count++;
+    setCookieValue("login_cnt", count);
+    console.log("로그인 횟수: " + count);
+}
+
+function login() {
+    const failedLoginCount = parseInt(getCookieValue("failed_login_cnt"));
+
+    if (failedLoginCount >= 3) {
+        console.log("로그인이 제한되었습니다.");
+        return;
+    }
+
+    login_count();
+
+    // 로그인 처리 추가 작업
+
+    // 세션 유지 시간 설정
+    setTimeout(logout, loginTimeout);
+}
+
+function logout() {
+    deleteCookie("login_cnt");
+    deleteCookie("failed_login_cnt");
+    console.log("자동 로그아웃 되었습니다.");
+    // 메인 페이지로 이동 추가 작업
+}
+
+function login_failed() {
+    let count = parseInt(getCookieValue("failed_login_cnt"));
+    count++;
+    setCookieValue("failed_login_cnt", count);
+    console.log("로그인 실패 횟수: " + count);
+
+    if (count >= 3) {
+        console.log("로그인이 제한되었습니다.");
+    }
+}
+
+function loginFailedDelay() {
+    setTimeout(login_failed, 5000); // 5초 지연 후 로그인 실패 처리
+}*/
+
+/*function login_check(id, password) {
+  const idPattern = /^[a-zA-Z0-9]{4,12}$/;
+  const passwordPattern = /^[a-zA-Z0-9!@#$%^&*()]{6,16}$/;
+
+  if (!idPattern.test(id) || !passwordPattern.test(password)) {
+    console.log("아이디와 패스워드 형식이 올바르지 않습니다.");
+    return false;
+  }
+
+  return true;
+}*/
